4386a66646d1657821222358375a7580137115b6
Albert Lee <leesga8@gmail.com>
2021-11-19 10:29:33 -0800

    add API key, .env, html bones

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import anime from 'animejs/lib/anime.es.js'
+
+$(document).ready(function(){
+  $('#getCurrency').click(function(){
+    const dollarAmount = $('#dollar').val();
+    $('#dollar').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send()
+
+    function getElements(response) {
+      $('.showExchange').text(`${response.conversion_rates}`);
+    }
+  });
+});
